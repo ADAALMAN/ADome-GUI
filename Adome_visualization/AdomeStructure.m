dc07d5ef649afb41c3f074fba0bf39848f38816f
ADAALMAN <Adaalman@outlook.com>
2022-10-04 13:53:15 +0200

    Added bucky v2.

--- Adome_visualization/AdomeStructure.m
+++ Adome_visualization/AdomeStructure.m
@@ -1,12 +1,20 @@
-function [x, y, z] = AdomeStructure()
-    %% Generate geodesic ADome structure
-    [A, xy] = bucky;         % Generate bucky matrix 
-    [i, j] = find(triu(A));  
-    B = permute(cat(3, xy(i, :),xy(j, :)), [3 1 2]);
+function AdomeStructure(app)
     
-    % Plot upper-part bucky 
-    length = 1:40;
-    x = B(:, length, 1);
-    y = B(:, length, 2);
-    z = B(:, length, 3);
+    [B, V] = bucky;
+    
+    for i = 1:60
+        for j = 1:60
+            if i == j
+                continue
+            end
+    
+            dist = (V(i,1)-V(j,1))^2 + (V(i,2)-V(j,2))^2 + (V(i,3)-V(j,3))^2;
+            
+            if (sqrt(dist) < 0.5) && V(i,3) > -0.3 && V(j,3) > -0.3
+                plot3(app.UIAxes, [V(i,1) V(j,1)], [V(i,2) V(j,2)], [V(i,3) V(j,3)], "r" )
+                hold (app.UIAxes, "on")
+            end
+    
+        end
+    end
 end
