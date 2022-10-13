3123e391856916c5e0d9d42b1e25e43b49e2bb85
ADAALMAN <Adaalman@outlook.com>
2022-10-13 12:35:36 +0200

    Constallation can be called independently, with just giving the axes, theta and phi.

--- Adome_visualization/AntennaConstellation.m
+++ Adome_visualization/AntennaConstellation.m
@@ -1,4 +1,4 @@
-function [x, y, z] = AntennaConstellation(TH, PH)
+function AntennaConstellation(axes, TH, PH)
     %tilt coordinates from x-axis to z-axis (original data was pointing to side)
     [X1,Y1,Z1] = angle2cart(PH*pi/180,TH*pi/180,1);
     
@@ -7,6 +7,19 @@ function [x, y, z] = AntennaConstellation(TH, PH)
     x = X1;
     y = Y1*cos(theta) - Z1*sin(theta);
     z = -(Y1*sin(theta) + Z1*cos(theta));
+    
+    for i = 1:length(TH)
+                id{i} = i;
+    end
+
+    nodes = plot3(axes, z, y, x, '.','markersize', 20);
+    dtRows = [dataTipTextRow("Id:",id),... 
+              dataTipTextRow("X:",z),... 
+              dataTipTextRow("Y:",y),... 
+              dataTipTextRow("Z:",x),...
+              dataTipTextRow("Theta:",TH),... 
+              dataTipTextRow("Phi:",PH)];
+    nodes.DataTipTemplate.DataTipRows = dtRows;
 end
 
 function [x,y,z] = angle2cart(th, ph, r, degRad)
