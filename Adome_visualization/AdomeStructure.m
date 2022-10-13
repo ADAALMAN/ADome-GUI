cae66499b2ad6052b7f145b73bdd5479193e3748
ADAALMAN <Adaalman@outlook.com>
2022-10-13 12:36:08 +0200

    Structure can be called independently with just giving the axes.

--- Adome_visualization/AdomeStructure.m
+++ Adome_visualization/AdomeStructure.m
@@ -1,5 +1,5 @@
-function AdomeStructure(app)
-    cla(app.UIAxes)
+function AdomeStructure(axes)
+    cla(axes)
     [B, V] = bucky;
     for i = 1:60
         for j = 1:60
@@ -10,13 +10,13 @@ function AdomeStructure(app)
             dist = (V(i,1)-V(j,1))^2 + (V(i,2)-V(j,2))^2 + (V(i,3)-V(j,3))^2;
             
             if (sqrt(dist) < 0.5) && V(i,3) > -0.6 && V(j,3) > -0.6
-                plot3(app.UIAxes, [V(i,1) V(j,1)], [V(i,2) V(j,2)], [V(i,3) V(j,3)], "r" )
-                hold (app.UIAxes, "on")
+                plot3(axes, [V(i,1) V(j,1)], [V(i,2) V(j,2)], [V(i,3) V(j,3)], "r" )
+                hold (axes, "on")
             end
             
             
             if (sqrt(dist) > 0.5 && sqrt(dist) < 0.7) && (V(i,3) > -0.6 && V(i,3) < -0.5) && (V(j,3) > -0.6 && V(j,3) < -0.5)
-                plot3(app.UIAxes, [V(i,1) V(j,1)], [V(i,2) V(j,2)], [V(i,3) V(j,3)], "r" )
+                plot3(axes, [V(i,1) V(j,1)], [V(i,2) V(j,2)], [V(i,3) V(j,3)], "r" )
             end
         end
     end
