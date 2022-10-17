5cb0b3330116ccaaed3f9bfcb4d2872badd5e409
ADAALMAN <Adaalman@outlook.com>
2022-10-17 18:34:16 +0200

    Seperated angle2cart function from constellation file.

--- Adome_visualization/AntennaConstellation.m
+++ Adome_visualization/AntennaConstellation.m
@@ -21,15 +21,3 @@ function AntennaConstellation(axes, TH, PH)
               dataTipTextRow("Phi:",PH)];
     nodes.DataTipTemplate.DataTipRows = dtRows;
 end
-
-function [x,y,z] = angle2cart(th, ph, r, degRad)
-if nargin > 3 
-    if strcmp(degRad,'deg')
-        ph = ph .* pi ./ 180;
-        th = th .* pi ./ 180;
-    end
-end
-x = r .* sin(th).* cos(ph);
-y = r .* sin(th).* sin(ph);
-z = r .* cos(th);
-end
