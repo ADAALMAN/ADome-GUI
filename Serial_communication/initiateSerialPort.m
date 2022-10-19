308e63cc0f4316b900febe79afad6e2a6907c912
ADAALMAN <Adaalman@outlook.com>
2022-10-19 13:17:41 +0200

    Changed directories

---
+++ Serial_communication/initiateSerialPort.m
@@ -0,0 +1,10 @@
+function s = initiateSerialPort(serialPortAddress, numberOfBoards)
+    % Create serialport with correct baud rate and timeout of X seconds
+    s = serialport(serialPortAddress, 115200, 'Timeout', 1);
+    configureTerminator(s,"CR/LF") % set end of line with '\r\n'
+    
+    %Set readout settings
+    write(s, ['b' numberOfBoards], "uint8"); % Set number of [b]oards
+    write(s, ['d' 20], "uint8"); % Set sample [d]elay in us
+    pause(1e-3);
+end
